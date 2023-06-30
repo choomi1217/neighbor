32db48299307ec37c74a05a0a58be2eb33d4c3ed
o_omi1217 <whdudal1217@naver.com>
2023-07-01 00:27:04 +0900

    init

---
+++ src/test/java/who/is/neighbor/NeighborApplicationTests.java
@@ -0,0 +1,13 @@
+package who.is.neighbor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighborApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
