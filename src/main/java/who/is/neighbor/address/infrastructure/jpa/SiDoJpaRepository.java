1c57f4734655a472e862b4d929cf2141ea29f431
o_omi1217 <whdudal1217@naver.com>
2023-07-22 22:00:53 +0900

    시도 수정

---
+++ src/main/java/who/is/neighbor/address/infrastructure/jpa/SiDoJpaRepository.java
@@ -0,0 +1,6 @@
+package who.is.neighbor.address.infrastructure.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SiDoJpaRepository extends JpaRepository<SiDoEntity, Long> {
+}
