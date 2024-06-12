971242c76cd6c702c28f26f36c706b9610c2fc95
youngmi <youngmi.cho@nflux.co.kr>
2024-06-12 17:11:17 +0900

    remove: 불필요한 임포트 제거

--- src/main/java/who/is/neighbor/config/SwaggerConfiguration.java
+++ src/main/java/who/is/neighbor/config/SwaggerConfiguration.java
@@ -3,14 +3,11 @@ package who.is.neighbor.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.properties.SwaggerUiConfigParameters;
-import org.springdoc.webmvc.ui.SwaggerWelcomeCommon;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfiguration {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
