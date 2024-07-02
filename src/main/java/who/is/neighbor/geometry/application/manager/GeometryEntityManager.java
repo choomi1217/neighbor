673d93b83c8d7fef0ced142476f71cc89769a48c
youngmi <youngmi.cho@nflux.co.kr>
2024-07-02 18:56:33 +0900

    fix: shpfile read 기능 추가

--- src/main/java/who/is/neighbor/geometry/application/manager/GeometryEntityManager.java
+++ src/main/java/who/is/neighbor/geometry/application/manager/GeometryEntityManager.java
@@ -9,25 +9,25 @@ import who.is.neighbor.address.infrastructure.GeometryRepository;
 
 import java.sql.SQLException;
 
-public interface GeometryEntityManager {
+public interface GeometryEntityManager<T> {
 
     @Transactional
-    default void insertTable(FeatureIterator<SimpleFeature> iterator, GeometryRepository<?> repository) throws SQLException {
+    default void insertTable(FeatureIterator<SimpleFeature> iterator, GeometryRepository<T> repository) throws SQLException {
         while (iterator.hasNext()) {
             SimpleFeature feature = iterator.next();
 
-            Object sido_id = feature.getAttribute("ctprvn_cd");
-            Object sido_name = feature.getAttribute("ctp_kor_nm");
+            Object id = feature.getAttribute("ctprvn_cd");
+            Object name = feature.getAttribute("ctp_kor_nm");
 
             Geometry geometry = (Geometry) feature.getDefaultGeometry();
             String wkt = geometry.toText();
             PGpolygon pgGeometry = new PGpolygon(wkt);
-            Object entity = createEntity(sido_id, sido_name, pgGeometry);
+            T entity = createEntity(id, name, pgGeometry);
             repository.save(entity);
         }
     }
 
     void insertTable(FeatureIterator<SimpleFeature> iterator) throws SQLException;
 
-    Object createEntity(Object id, Object name, PGpolygon geometry);
+    T createEntity(Object id, Object name, PGpolygon geometry);
 }
